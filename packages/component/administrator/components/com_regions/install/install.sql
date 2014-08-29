8187e78809ff8504b3f10d60d98877faf54698c7
Jasper van Rijbroek <jasper@jvar.nl>
2014-08-29 09:42:26 +0200

    Added the installation of the french database to the install.sql file.

--- packages/component/administrator/components/com_regions/install/install.sql
+++ packages/component/administrator/components/com_regions/install/install.sql
@@ -20,6 +20,25 @@ CREATE TABLE IF NOT EXISTS `#__regions_regions` (
   PRIMARY KEY (`regions_region_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `#__fr_regions_regions` (
+  `regions_region_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` char(36) NOT NULL,
+  `cck_fieldset_id` int(11) NOT NULL DEFAULT '1900',
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL DEFAULT 'region',
+  `fields` longtext NULL,
+  `ordering` bigint(20) unsigned NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
+  `created_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created_by` int(11) NOT NULL DEFAULT '0',
+  `modified_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified_by` int(11) NOT NULL DEFAULT '0',
+  `locked_on` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `locked_by` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`regions_region_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 -- Insert regions into fieldsets
 INSERT INTO `#__cck_fieldsets` (`cck_fieldset_id`, `title`, `slug`, `enabled`) VALUES (1900, 'Region', 'region', 1);
 
