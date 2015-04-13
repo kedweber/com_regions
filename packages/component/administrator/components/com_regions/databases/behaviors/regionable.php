9e5b9b17e47adc89ce9f52d9acb36ebaf2cb1770
Jasper van Rijbroek <jasper@jvar.nl>
2015-04-13 12:06:14 +0200

    Updated regionable.

--- packages/component/administrator/components/com_regions/databases/behaviors/regionable.php
+++ packages/component/administrator/components/com_regions/databases/behaviors/regionable.php
@@ -5,7 +5,7 @@
  * if a user isn't then the user will be checked for groups and regions, if none are present, then you don't have permissions.
  * If a user has regions/ groups then these will be applied as a filter.
  * 
- * Also if a user has groups, it can only select groups from which it is a member.
+ * Also if a user has regions, it can only select regions from which it is a member.
  */
 
 class ComRegionsDatabaseBehaviorRegionable extends KDatabaseBehaviorAbstract
@@ -17,16 +17,8 @@ class ComRegionsDatabaseBehaviorRegionable extends KDatabaseBehaviorAbstract
         $user = JFactory::getUser();
 
         if(!$user->get('isRoot')) {
-            $groups = $this->getService('com://admin/profile.model.groups')->user_id($user->id)->getList();
+            $groups = $this->getService('com://admin/profile.model.groups')->user_id($user->id)->getList()->getRegions();
             $regions = array();
-
-            foreach ($groups as $group) {
-                foreach ($group->regions as $region) {
-                    if (!in_array($region->id, $regions)) {
-                        $regions[] = $region->id;
-                    }
-                }
-            }
 
             foreach ($regions as $region) {
                 $context->query->where('FIND_IN_SET(' . $region . ', REPLACE(SUBSTRING_INDEX(SUBSTR(ANCESTORS,LOCATE(\'"REGIONS":[\',ANCESTORS)+CHAR_LENGTH(\'"REGIONS":[\')),\']\', 1),\'\', \'\'))', null, null, 'OR');
